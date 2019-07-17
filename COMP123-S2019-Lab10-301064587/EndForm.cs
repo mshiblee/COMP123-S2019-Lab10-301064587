f567f70b2b3ba259617bb37f36b9be22d1cd7428
Muhammad Shiblee <m.shiblee@outlook.com>
2019-07-17 11:57:55 -0400

    Created Lab 10 - Multiple forms.

---
+++ COMP123-S2019-Lab10-301064587/EndForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_S2019_Lab10_301064587
+{
+    public partial class EndForm : Form
+    {
+        public EndForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+            Program.mainForm.Show();
+            Program.endForm.Hide();
+        }
+
+        private void EndForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
